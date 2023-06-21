0eca08cc2e5797a0961a4785ce2aea19e99e7eef
Lea Vauchier <lea.vauchier@ign.fr>
2023-06-21 14:17:48 +0200

    Run test_epsg_fail locally

--- test/test_color.py
+++ test/test_color.py
@@ -19,13 +19,15 @@ def setup_module(module):
         pass
     os.mkdir(TMPDIR)
 
-input_file = "/var/data/store-lidarhd/developpement/lidarexpress/tests/test_bug_laz_epsg/epsg_fail/Semis_2021_0435_6292_LA93_IGN69.laz"
-ouptput_file = TMPDIR + "Semis_2021_0435_6292_LA93_IGN69.las"
+TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+INPUT_PATH = os.path.join(TEST_PATH, 'data/test_noepsg_043500_629205_IGN69.laz')
+
+OUTPUT_FILE = TMPDIR + "Semis_2021_0435_6292_LA93_IGN69.las"
 
 
 def test_epsg_fail():
     with pytest.raises(requests.exceptions.HTTPError, match="400 Client Error: BadRequest for url") :
-        color.decomp_and_color(input_file, ouptput_file, "", 0.1, 15)
+        color.decomp_and_color(INPUT_PATH, OUTPUT_FILE, "", 0.1, 15)
 
 
 epsg = "2154"
@@ -38,19 +40,19 @@ pixel_per_meter=0.1
 
 
 def test_download_image_ok():
-    color.download_image_from_geoportail(epsg, layer, minx, miny, maxx, maxy, pixel_per_meter, ouptput_file, 15)
+    color.download_image_from_geoportail(epsg, layer, minx, miny, maxx, maxy, pixel_per_meter, OUTPUT_FILE, 15)
 
 
 def test_download_image_raise1():
     retry_download = color.retry(2, 5)(color.download_image_from_geoportail)
     with pytest.raises(requests.exceptions.HTTPError):
-        retry_download(epsg, "MAUVAISE_COUCHE", minx, miny, maxx, maxy, pixel_per_meter, ouptput_file, 15)
+        retry_download(epsg, "MAUVAISE_COUCHE", minx, miny, maxx, maxy, pixel_per_meter, OUTPUT_FILE, 15)
 
 
 def test_download_image_raise2():
     retry_download = color.retry(2, 5)(color.download_image_from_geoportail)
     with pytest.raises(requests.exceptions.HTTPError):
-        retry_download("9001", layer, minx, miny, maxx, maxy, pixel_per_meter, ouptput_file, 15)
+        retry_download("9001", layer, minx, miny, maxx, maxy, pixel_per_meter, OUTPUT_FILE, 15)
 
 
 def test_retry_on_server_error():
@@ -58,7 +60,7 @@ def test_retry_on_server_error():
         mock.get(requests_mock.ANY, status_code=502, reason="Bad Gateway")
         with pytest.raises(requests.exceptions.HTTPError):
             retry_download = color.retry(2, 1, 2)(color.download_image_from_geoportail)
-            retry_download(epsg, layer, minx, miny, maxx, maxy, pixel_per_meter, ouptput_file, 15)
+            retry_download(epsg, layer, minx, miny, maxx, maxy, pixel_per_meter, OUTPUT_FILE, 15)
         history = mock.request_history
         assert len(history) == 3
 
@@ -68,7 +70,7 @@ def test_retry_on_connection_error():
         mock.get(requests_mock.ANY, exc=requests.exceptions.ConnectionError)
         with pytest.raises(requests.exceptions.ConnectionError):
             retry_download = color.retry(2, 1)(color.download_image_from_geoportail)
-            retry_download(epsg, layer, minx, miny, maxx, maxy, pixel_per_meter, ouptput_file, 15)
+            retry_download(epsg, layer, minx, miny, maxx, maxy, pixel_per_meter, OUTPUT_FILE, 15)
 
         history = mock.request_history
         assert len(history) == 3
