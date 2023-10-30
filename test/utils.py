6b1876e94fff7bac8dacccecb9421ad8c3ec1fc9
Lea Vauchier <lea.vauchier@ign.fr>
2023-10-30 16:34:39 +0100

    Update pdal to 2.6+ and adapt tests

--- test/utils.py
+++ test/utils.py
@@ -9,3 +9,55 @@ def get_pdal_infos_summary(f: str):
     r = sp.run(["pdal", "info", "--summary", f], stderr=sp.PIPE, stdout=sp.PIPE)
     json_info = json.loads(r.stdout.decode())
     return json_info
+
+
+EXPECTED_DIMS_BY_DATAFORMAT = {
+    6: set(
+        [
+            "X",
+            "Y",
+            "Z",
+            "Intensity",
+            "ReturnNumber",
+            "NumberOfReturns",
+            "ScanChannel",
+            "ScanDirectionFlag",
+            "EdgeOfFlightLine",
+            "Classification",
+            "UserData",
+            "ScanAngleRank",
+            "PointSourceId",
+            "GpsTime",
+            "KeyPoint",
+            "Overlap",
+            "Synthetic",
+            "Withheld",
+        ]
+    ),
+    8: set(
+        [
+            "X",
+            "Y",
+            "Z",
+            "Intensity",
+            "ReturnNumber",
+            "NumberOfReturns",
+            "ScanChannel",
+            "ScanDirectionFlag",
+            "EdgeOfFlightLine",
+            "Classification",
+            "UserData",
+            "ScanAngleRank",
+            "PointSourceId",
+            "GpsTime",
+            "Red",
+            "Green",
+            "Blue",
+            "Infrared",
+            "KeyPoint",
+            "Overlap",
+            "Synthetic",
+            "Withheld",
+        ]
+    ),
+}
