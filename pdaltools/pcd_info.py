0a05bd053d23720be6703c6abd73e9651410cb83
Lea Vauchier <lea.vauchier@ign.fr>
2024-08-22 14:53:37 +0200

    Clean up get_origin_pointcloud

--- pdaltools/pcd_info.py
+++ pdaltools/pcd_info.py
@@ -5,7 +5,9 @@ from typing import Tuple
 import numpy as np
 
 
-def get_pointcloud_origin_from_tile_width(points: np.array, tile_width: int = 1000, buffer_size: float = 0) -> Tuple:
+def get_pointcloud_origin_from_tile_width(
+    points: np.ndarray, tile_width: int = 1000, buffer_size: float = 0
+) -> Tuple[int, int]:
     """Get point cloud theoretical origin (xmin, ymax) for a data that originates from a square tesselation/tiling
     using the tesselation tile width only.
 
@@ -13,7 +15,7 @@ def get_pointcloud_origin_from_tile_width(points: np.array, tile_width: int = 10
 
 
     Args:
-        points (np.array): numpy array with the tile points
+        points (np.ndarray): numpy array with the tile points
         tile_width (int, optional): Edge size of the square used for tiling. Defaults to 1000.
         buffer_size (float, optional): Optional buffer around the tile. Defaults to 0.
 
@@ -21,23 +23,21 @@ def get_pointcloud_origin_from_tile_width(points: np.array, tile_width: int = 10
         ValueError: Raise an error when the bounding box of the tile is not included in a tile
 
     Returns:
-        Tuple: (origin_x, origin_y) origin coordinates
+        Tuple[int, int]: (origin_x, origin_y) origin coordinates
     """
     # Extract coordinates xmin, xmax, ymin and ymax of the original tile without buffer
     x_min, y_min = np.min(points[:, :2], axis=0) + buffer_size
     x_max, y_max = np.max(points[:, :2], axis=0) - buffer_size
 
-    # Calculate the difference Xmin and Xmax, then Ymin and Ymax
-    diff_tile_x = np.floor(x_max / tile_width) - np.floor(x_min / tile_width)
-    is_x_max_on_edge = x_max % tile_width == 0
-    diff_tile_y = np.ceil(y_max / tile_width) - np.ceil(y_min / tile_width)
-    is_y_min_on_edge = y_min % tile_width == 0
-    # Check [x_min - x_max] == amplitude and [y_min - y_max] == amplitude
-    if (diff_tile_x == 0 or (diff_tile_x == 1 and is_x_max_on_edge)) and (
-        diff_tile_y == 0 or (diff_tile_y == 1 and is_y_min_on_edge)
-    ):
-        origin_x = np.floor(x_min / tile_width) * tile_width  # round low
-        origin_y = np.ceil(y_max / tile_width) * tile_width  # round top
+    # Calculate the tiles to which x, y bounds belong
+    tile_x_min = np.floor(x_min / tile_width)
+    tile_x_max = np.floor(x_max / tile_width) if x_max % tile_width != 0 else np.floor(x_max / tile_width) - 1
+    tile_y_min = np.ceil(y_min / tile_width) if y_min % tile_width != 0 else np.floor(y_min / tile_width) + 1
+    tile_y_max = np.ceil(y_max / tile_width)
+
+    if not (tile_x_max - tile_x_min) and not (tile_y_max - tile_y_min):
+        origin_x = tile_x_min * tile_width
+        origin_y = tile_y_max * tile_width
         return origin_x, origin_y
     else:
         raise ValueError(
