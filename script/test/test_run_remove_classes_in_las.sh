729dd0119370cbcc21aa5a23f4b9c5f28ec1343d
Antoine Lavenant <Antoine.Lavenant@ign.fr>
2024-09-13 13:49:11 +0200

    add fn to remove points during standardisation

---
+++ script/test/test_run_remove_classes_in_las.sh
@@ -0,0 +1,5 @@
+python -m pdaltools.standardize_format \
+    --input_file test/data/classified_laz/test_data_77050_627755_LA93_IGN69.laz \
+    --output_file test/tmp/replaced_cmdline.laz \
+    --record_format 6 \
+    --class_points_removed 2 \
--- test/test_las_remove_dimensions.py
+++ test/test_las_remove_dimensions.py
@@ -13,16 +13,22 @@ INPUT_DIR = os.path.join(TEST_PATH, "data")
 ini_las = os.path.join(INPUT_DIR, "test_data_77055_627760_LA93_IGN69.laz")
 added_dimensions = ["DIM_1", "DIM_2"]
 
-def get_points(input_las : str):
+
+def get_points(input_las: str):
     pipeline_read_ini = pdal.Pipeline() | pdal.Reader.las(input_las)
     pipeline_read_ini.execute()
     return pipeline_read_ini.arrays[0]
 
-def append_dimension(input_las : str, output_las : str):
+
+def append_dimension(input_las: str, output_las: str):
     pipeline = pdal.Pipeline()
     pipeline |= pdal.Reader.las(input_las)
     pipeline |= pdal.Filter.ferry(dimensions="=>" + ", =>".join(added_dimensions))
-    pipeline |= pdal.Writer.las(output_las, extra_dims="all", forward="all", )
+    pipeline |= pdal.Writer.las(
+        output_las,
+        extra_dims="all",
+        forward="all",
+    )
     pipeline.execute()
 
 
@@ -52,10 +58,9 @@ def test_remove_one_dimension():
             las_remove_dimensions.remove_dimensions_from_las(tmp_las.name, ["DIM_1"], tmp_las_rm.name)
             points_end = get_points(tmp_las_rm.name)
 
-            assert list(points_end.dtype.fields.keys()).index("DIM_2") >= 0# should still contains DIM_2
+            assert list(points_end.dtype.fields.keys()).index("DIM_2") >= 0  # should still contains DIM_2
 
-            with pytest.raises(ValueError):
-                list(points_end.dtype.fields.keys()).index("DIM_1") # should not have DIM_1
+            assert "DIM_1" not in points_end.dtype.fields.keys(), "LAS should not have dimension DIM_1"
 
             with pytest.raises(TypeError):
                 numpy.array_equal(points_ini, points_end)  # output data should not be the same
