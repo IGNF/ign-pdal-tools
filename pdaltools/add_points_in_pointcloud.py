bcf4cf6ae6e748adfae0890b8e4e4e920e4f0732
mdupays <malvina.dupays@ign.fr>
2025-01-28 17:00:47 +0100

    add function : add points in pointclouds

--- pdaltools/add_points_in_pointcloud.py
+++ pdaltools/add_points_in_pointcloud.py
@@ -6,7 +6,7 @@ from shapely.geometry import box
 from pdaltools.las_info import get_tile_origin_using_header_info
 
 
-def get_tile_bbox(input_las, tile_width=1000):
+def get_tile_bbox(input_las, tile_width=1000) -> tuple:
     """
     Get the theoretical bounding box (xmin, ymin, xmax, ymax) of a LIDAR tile
     using its origin and the predefined tile width.
@@ -23,51 +23,7 @@ def get_tile_bbox(input_las, tile_width=1000):
     return bbox
 
 
-def add_points_to_las(
-    input_points_with_z: gpd.GeoDataFrame, input_las: str, virtual_points_classes: int, output_las: str
-):
-    """Add points (3D points in LAZ format) by LIDAR tiles (tiling file)
-
-    Args:
-        input_points_with_z(gpd.GeoDataFrame): geometry columns (2D points) as encoded to WKT.
-        input_las (str): Path to the LIDAR tiles (LAZ).
-        virtual_points_classes (int): The classification value to assign to those virtual points.
-        output_las (str): Path to save the updated LIDAR file (LAS/LAZ format).
-    """
-    # Extract XYZ coordinates and additional attribute (classification)
-    x_coords = input_points_with_z.geometry.x
-    y_coords = input_points_with_z.geometry.y
-    z_coords = input_points_with_z.RecupZ
-    classes = virtual_points_classes * np.ones(len(input_points_with_z.index))
-
-    # Read the existing LIDAR file
-    with laspy.open(input_las, mode="r") as las:
-        las_data = las.read()
-        header = las.header
-
-        # Create a new header if the original header is missing or invalid
-        if header is None:
-            header = laspy.LasHeader(point_format=6, version="1.4")  # Example format and version
-
-        # Append the clipped points to the existing LIDAR data
-        new_x = np.concatenate([las_data.x, x_coords])
-        new_y = np.concatenate([las_data.y, y_coords])
-        new_z = np.concatenate([las_data.z, z_coords])
-        new_classes = np.concatenate([las_data.classification, classes])
-
-        # Create a new LAS file with updated data
-        updated_las = laspy.LasData(header)
-        updated_las.x = new_x
-        updated_las.y = new_y
-        updated_las.z = new_z
-        updated_las.classification = new_classes
-
-        # Write the updated LAS file
-        with laspy.open(output_las, mode="w", header=header, do_compress=True) as writer:
-            writer.write_points(updated_las.points)
-
-
-def clip_3d_points_to_tile(input_points: str, input_las: str, crs: str, output_las: str):
+def clip_3d_points_to_tile(input_points: str, input_las: str, crs: str) -> gpd.GeoDataFrame:
     """
     Add points from a GeoJSON file in the LIDAR's tile.
 
@@ -75,10 +31,9 @@ def clip_3d_points_to_tile(input_points: str, input_las: str, crs: str, output_l
         input_points (str): Path to the input GeoJSON file with 3D points.
         input_las (str): Path to the LIDAR `.las/.laz` file.
         crs (str): CRS of the data, e.g., 'EPSG:2154'.
-        output_las (str): Path to save the updated LIDAR file (LAS/LAZ format).
 
-    Returns:
-        gpd.GeoDataFrame: geometry columns as encoded to WKT
+    Return:
+        gpd.GeoDataFrame: Points 2d with "Z" value
     """
     # Compute the bounding box of the LIDAR tile
     tile_bbox = get_tile_bbox(input_las)
@@ -96,6 +51,50 @@ def clip_3d_points_to_tile(input_points: str, input_las: str, crs: str, output_l
     # Clip the points to the bounding box
     clipped_points = points_gdf[points_gdf.intersects(bbox_polygon)].copy()
 
-    # Add points with Z in pointcloud
-    if not clipped_points.empty:
-        add_points_to_las(clipped_points, input_las, 66, output_las)
+    return clipped_points
+
+
+def add_points_to_las(
+    input_points_with_z: gpd.GeoDataFrame, input_las: str, output_las: str, virtual_points_classes=66
+):
+    """Add points (3D points in LAZ format) by LIDAR tiles (tiling file)
+
+    Args:
+        input_points_with_z(gpd.GeoDataFrame): geometry columns (2D points) as encoded to WKT.
+        input_las (str): Path to the LIDAR tiles (LAZ).
+        output_las (str): Path to save the updated LIDAR file (LAS/LAZ format).
+        virtual_points_classes (int): The classification value to assign to those virtual points (default: 66).
+    """
+    # Check points with Z are not empty
+    if not input_points_with_z.empty:
+        # Extract XYZ coordinates and additional attribute (classification)
+        x_coords = input_points_with_z.geometry.x
+        y_coords = input_points_with_z.geometry.y
+        z_coords = input_points_with_z.RecupZ
+        classes = virtual_points_classes * np.ones(len(input_points_with_z.index))
+
+        # Read the existing LIDAR file
+        with laspy.open(input_las, mode="r") as las:
+            las_data = las.read()
+            header = las.header
+
+            # Create a new header if the original header is missing or invalid
+            if header is None:
+                header = laspy.LasHeader(point_format=6, version="1.4")  # Example format and version
+
+            # Append the clipped points to the existing LIDAR data
+            new_x = np.concatenate([las_data.x, x_coords])
+            new_y = np.concatenate([las_data.y, y_coords])
+            new_z = np.concatenate([las_data.z, z_coords])
+            new_classes = np.concatenate([las_data.classification, classes])
+
+            # Create a new LAS file with updated data
+            updated_las = laspy.LasData(header)
+            updated_las.x = new_x
+            updated_las.y = new_y
+            updated_las.z = new_z
+            updated_las.classification = new_classes
+
+            # Write the updated LAS file
+            with laspy.open(output_las, mode="w", header=header, do_compress=True) as writer:
+                writer.write_points(updated_las.points)
--- test/test_add_points_in_pointcloud.py
+++ test/test_add_points_in_pointcloud.py
@@ -1,17 +1,34 @@
 import os
+from pathlib import Path
 
 from pdaltools import add_points_in_pointcloud
 
 TEST_PATH = os.path.dirname(os.path.abspath(__file__))
 TMP_PATH = os.path.join(TEST_PATH, "data/output")
-DATA_LIDAR_PATH = os.path.join(TEST_PATH, "data/pointcloud")
+DATA_LIDAR_PATH = os.path.join(TEST_PATH, "data/classified_laz")
 DATA_POINTS_PATH = os.path.join(TEST_PATH, "data/points_3d")
 
-INPUT_FILE = os.path.join(DATA_LIDAR_PATH, "Semis_2023_0292_6833_LA93_IGN69.laz")
+INPUT_FILE = os.path.join(DATA_LIDAR_PATH, "test_semis_2023_0292_6833_LA93_IGN69.laz")
 INPUT_POINTS = os.path.join(DATA_POINTS_PATH, "Points_virtuels.geojson")
-OUTPUT_FILE = os.path.join(TMP_PATH, "Semis_2023_0292_6833_LA93_IGN69.laz")
+OUTPUT_FILE = os.path.join(TMP_PATH, "test_semis_2023_0292_6833_LA93_IGN69.laz")
 
 
-def test_test_add_line_to_lidar():
-    print(DATA_POINTS_PATH)
-    add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, "EPSG:2154", OUTPUT_FILE)
+def setup_module(module):
+    os.makedirs("test/data/output", exist_ok=True)
+
+
+def test_get_tile_bbox():
+    bbox = add_points_in_pointcloud.get_tile_bbox(INPUT_FILE, 1000)
+    assert bbox == (292000.0, 6832000.0, 293000.0, 6833000.0)
+
+
+def test_clip_3d_points_to_tile():
+    points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, "EPSG:2154")
+    assert len(points_clipped) == 678
+
+
+def test_add_line_to_lidar():
+    points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, "EPSG:2154")
+
+    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE, OUTPUT_FILE, 66)
+    assert Path(OUTPUT_FILE).exists()
