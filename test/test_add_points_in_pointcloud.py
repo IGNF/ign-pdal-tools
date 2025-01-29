9c8ee182babee5e6d307fd6fe6dc91710788720c
mdupays <malvina.dupays@ign.fr>
2025-01-29 09:56:37 +0100

    modify test_add_line_to_lidar : check classes

--- test/test_add_points_in_pointcloud.py
+++ test/test_add_points_in_pointcloud.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import pdal
+
 from pdaltools import add_points_in_pointcloud
 
 TEST_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -19,16 +21,30 @@ def setup_module(module):
 
 def test_get_tile_bbox():
     bbox = add_points_in_pointcloud.get_tile_bbox(INPUT_FILE, 1000)
-    assert bbox == (292000.0, 6832000.0, 293000.0, 6833000.0)
+    assert bbox == (292000.0, 6832000.0, 293000.0, 6833000.0)  # check the bbox from LIDAR tile
 
 
 def test_clip_3d_points_to_tile():
     points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, "EPSG:2154")
-    assert len(points_clipped) == 678
+    assert len(points_clipped) == 678  # chech the entity's number of points
 
 
 def test_add_line_to_lidar():
     points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, "EPSG:2154")
 
-    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE, OUTPUT_FILE, 66)
-    assert Path(OUTPUT_FILE).exists()
+    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE, OUTPUT_FILE, 68)
+    assert Path(OUTPUT_FILE).exists()  # check output exists
+
+    # Filter pointcloud by classes
+    pipeline = (
+        pdal.Reader.las(filename=OUTPUT_FILE, nosrs=True)
+        | pdal.Filter.range(
+            limits="Classification[68:68]",
+        )
+        | pdal.Filter.stats()
+    )
+    pipeline.execute()
+    metadata = pipeline.metadata
+    # Count the pointcloud's number from classe "68"
+    point_count = metadata["metadata"]["filters.stats"]["statistic"][0]["count"]
+    assert point_count == 678
