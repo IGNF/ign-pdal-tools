28653900056c4c952097397c23773788e2d33f88
mdupays <malvina.dupays@ign.fr>
2025-02-19 14:50:08 +0100

    refacto arguments and severals functions from add_points_inpointcloud.py: spatial_ref
    Update CHANGELOG.md
    
    Co-authored-by: leavauchier <120112647+leavauchier@users.noreply.github.com>
    
    refacto function add_points_in_pointclouds : arguments, add function epsg, etc
    
    add new version

--- test/test_add_points_in_pointcloud.py
+++ test/test_add_points_in_pointcloud.py
@@ -30,14 +30,41 @@ def test_get_tile_bbox():
 
 
 def test_clip_3d_points_to_tile():
+    # With EPSG
     points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, "EPSG:2154", 1000)
+    assert len(points_clipped) == 678  # chech the entity's number of points
+
+
+def test_clip_3d_points_to_tile_from_epsg_none():
+    points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, None, 1000)
     assert len(points_clipped) == 678  # chech the entity's number of points
 
 
 def test_add_line_to_lidar():
     points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, "EPSG:2154", 1000)
 
-    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE, OUTPUT_FILE, 68)
+    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE, OUTPUT_FILE, "EPSG:2154", 68)
+    assert Path(OUTPUT_FILE).exists()  # check output exists
+
+    # Filter pointcloud by classes
+    pipeline = (
+        pdal.Reader.las(filename=OUTPUT_FILE, nosrs=True)
+        | pdal.Filter.range(
+            limits="Classification[68:68]",
+        )
+        | pdal.Filter.stats()
+    )
+    pipeline.execute()
+    metadata = pipeline.metadata
+    # Count the pointcloud's number from classe "68"
+    point_count = metadata["metadata"]["filters.stats"]["statistic"][0]["count"]
+    assert point_count == 678
+
+
+def test_add_line_to_lidar_from_epsg_none():
+    points_clipped = add_points_in_pointcloud.clip_3d_points_to_tile(INPUT_POINTS, INPUT_FILE, None, 1000)
+
+    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE, OUTPUT_FILE, None, 68)
     assert Path(OUTPUT_FILE).exists()  # check output exists
 
     # Filter pointcloud by classes
@@ -67,7 +94,7 @@ def test_add_line_to_lidar_small():
         INPUT_POINTS_SMALL, INPUT_FILE_SMALL, "EPSG:2154", 1000
     )
 
-    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE_SMALL, OUTPUT_FILE_SMALL, 68)
+    add_points_in_pointcloud.add_points_to_las(points_clipped, INPUT_FILE_SMALL, OUTPUT_FILE_SMALL, "EPSG:2154", 68)
     assert Path(OUTPUT_FILE).exists()  # check output exists
 
     # Filter pointcloud by classes
@@ -83,6 +110,20 @@ def test_add_line_to_lidar_small():
     # Count the pointcloud's number from classe "68"
     point_count = metadata["metadata"]["filters.stats"]["statistic"][0]["count"]
     assert point_count == 186
+
+
+def test_add_points_from_geojson_to_las():
+    add_points_in_pointcloud.add_points_from_geojson_to_las(
+        INPUT_POINTS, INPUT_FILE, OUTPUT_FILE, 68, "EPSG:2154", 1000
+    )
+    assert Path(OUTPUT_FILE).exists()  # check output exists
+
+
+def test_add_points_from_geojson_to_las_from_epsg_none():
+    add_points_in_pointcloud.add_points_from_geojson_to_las(
+        INPUT_POINTS, INPUT_FILE, OUTPUT_FILE, 68, None, 1000
+    )
+    assert Path(OUTPUT_FILE).exists()  # check output exists
 
 
 def test_parse_args():
